49e97a2e1190404f45adb3d4907d683aad4ac8e0
Moh Seyam <116368645+MohSeyam@users.noreply.github.com>
2025-07-25 05:20:05 +0300

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,16 +1,16 @@
+import Sidebar from "../components/layout/Sidebar";
 import Navbar from "../components/layout/Navbar";
 import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen bg-white bg-gradient-to-tr from-cyan-100 via-violet-100 to-amber-100 dark:from-zinc-900 dark:via-zinc-800 dark:to-zinc-900">
-      <div className="flex min-h-screen">
-        <div className="flex-1 flex flex-col">
-          <Navbar />
-          <main className="flex-1 p-4">
-            <Outlet />
-          </main>
-        </div>
+    <div className="min-h-screen flex bg-white dark:bg-dark-background">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+        <main className="flex-1 p-4">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
